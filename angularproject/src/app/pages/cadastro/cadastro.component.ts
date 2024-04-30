f460e74d8940758ffedcda8dba0584d5c965eef0
ilailsonrocha <ilailsonrocha@iec.gov.br>
2024-04-30 17:57:16 -0300

    Cadastrando funcionario

---
+++ angularproject/src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Funcionario } from 'src/app/models/Funcionarios';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent {
+
+  /**
+   *
+   */
+  constructor(private funcionarioService: FuncionarioService, private router: Router) {
+  }
+
+  createFuncionario(funcionario: Funcionario){
+    this.funcionarioService.createFuncionario(funcionario).subscribe((data)=>{
+      this.router.navigate(['/'])
+    })
+  }
+
+}
